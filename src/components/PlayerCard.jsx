5c6d482ac63f82060db411a71f29fd529b03ef46
galiphasandugan <galiphasandugan@gmail.com>
2023-07-23 18:14:41 +0200

    finally

--- src/components/PlayerCard.jsx
+++ src/components/PlayerCard.jsx
@@ -1,11 +1,22 @@
-import Col from 'react-bootstrap/Col';
+import Col from "react-bootstrap/Col";
+import Card from 'react-bootstrap/Card';
+import { useState } from "react";
 
-const PlayerCard = ({name,statistics, img}) => {
-  return (
-    <div>
-      <Col>{name}</Col>
-    </div>
-  )
-}
+const PlayerCard = ({ name, statistics, img }) => {
+  const [showImage, setShowImage] = useState(true)
+  return (          
+    <Col>
+      <Card>
+        <Card.Img variant="top" src={img} height="350px"/>
+        <ul>
+          {statistics.map((item, i) => <li className="list-unstyled h5 text-start" key={i}>{item}</li>)}
+        </ul>
+        <Card.Footer>
+           <Card.Title>{name}</Card.Title>
+        </Card.Footer>
+      </Card>
+    </Col>
+  );
+};
 
-export default PlayerCard
+export default PlayerCard;
