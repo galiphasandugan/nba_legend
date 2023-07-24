ab15df4051b808e9ff3a5c2b5d7118234f393925
galiphasandugan <galiphasandugan@gmail.com>
2023-07-24 11:52:42 +0200

    finished

--- src/components/PlayerCard.jsx
+++ src/components/PlayerCard.jsx
@@ -1,18 +1,74 @@
+import React, { useState } from "react";
 import Col from "react-bootstrap/Col";
-import Card from 'react-bootstrap/Card';
-import { useState } from "react";
+import Card from "react-bootstrap/Card";
 
-const PlayerCard = ({ name, statistics, img }) => {
-  const [showImage, setShowImage] = useState(true)
-  return (          
+const PlayerCard = ({ name, img, statistics }) => {
+  const [showImage, setShowImage] = useState(true);
+  // && || ?:
+  return (
     <Col>
-      <Card>
-        <Card.Img variant="top" src={img} height="350px"/>
-        <ul>
-          {statistics.map((item, i) => <li className="list-unstyled h5 text-start" key={i}>{item}</li>)}
-        </ul>
+      <Card onClick={()=>setShowImage(!showImage)} className="player-card" role="button">
+        {showImage ? (
+          <Card.Imgimport React, { useState } from "react";
+          import Col from "react-bootstrap/Col";
+          import Card from "react-bootstrap/Card";
+          
+          const PlayerCard = ({ name, img, statistics }) => {
+            const [showImage, setShowImage] = useState(true);
+            // && || ?:
+            return (
+              <Col>
+                <Card onClick={()=>setShowImage(!showImage)} className="player-card" role="button">
+                  {showImage ? (
+                    <Card.Img
+                      variant="top"
+                      src={img}
+                      height="350px"
+                      // onClick={() => setShowImage(false)}
+                    />
+                  ) : (
+                    <ul 
+                    className="m-auto"
+                  //   onClick={() => setShowImage(true)}
+                    >
+                      {statistics.map((item, i) => (
+                        <li className="list-unstyled h5 text-start" key={i}>
+                          {" "}
+                          🏀 {item}
+                        </li>
+                      ))}
+                    </ul>
+                  )}
+                  <Card.Footer>
+                    <Card.Title>{name}</Card.Title>
+                  </Card.Footer>
+                </Card>
+              </Col>
+            );
+          };
+          
+          export default PlayerCard;
+          
+            variant="top"
+            src={img}
+            height="350px"
+            // onClick={() => setShowImage(false)}
+          />
+        ) : (
+          <ul 
+          className="m-auto"
+        //   onClick={() => setShowImage(true)}
+          >
+            {statistics.map((item, i) => (
+              <li className="list-unstyled h5 text-start" key={i}>
+                {" "}
+                🏀 {item}
+              </li>
+            ))}
+          </ul>
+        )}
         <Card.Footer>
-           <Card.Title>{name}</Card.Title>
+          <Card.Title>{name}</Card.Title>
         </Card.Footer>
       </Card>
     </Col>
